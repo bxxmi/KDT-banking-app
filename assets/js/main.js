f33c0501e570eef7592587d084a327c25ff3d59e
bxxmi <bmi6638@gmail.com>
2021-09-15 17:55:33 +0900

    feat: 슬라이드 버튼 이벤트 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// 데이터 가져오기
 const bankingURL = 'https://syoon0624.github.io/json/test.json';
 const request = new XMLHttpRequest();
 
@@ -10,3 +11,10 @@ request.onload = () => {
   console.log(bankingData);
 }
 
+// 슬라이드 이벤트
+const btn = document.querySelector('.up-button'); 
+const bg = document.querySelector('.transaction');
+
+btn.addEventListener('click', () => {
+  bg.classList.toggle('up');
+});
