763c5340f8a159861562d4db9abce11e299943f3
bxxmi <bmi6638@gmail.com>
2021-09-15 18:35:11 +0900

    fix: 바 차트 속성 추가
    maintainAspectRatio 항목 추가했습니다.

--- assets/js/management.js
+++ assets/js/management.js
@@ -1,18 +1,27 @@
+// 바 차트
 const canvas1 = document.querySelector('#bar-chart');
 const barChart = new Chart(canvas1, {
   type: 'bar',
   data: {
-    labels: ['02', '04', '06', '08', '10'],
+    labels: 
+    ['02', '04', '06', '08', '10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30'],
     datasets: [{
-      backgroundColor: ['rgba(255, 92, 200, 0.5)',
-        'rgba(54, 162, 235, 0.5)', 'rgba(255, 206, 86, 0.5)',
-        'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)',
-        'rgba(255, 159, 64, 0.5)'
-      ],
-      data: [20000, 40000, 60000, 80000, 100000]
+      backgroundColor: '#38C976',
+      data:
+      [20000, 40000, 60000, 80000, 100000]
     }]
   },
   options: {
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 20000,
+          max: 100000,
+          stepSize: 20000
+        }
+      }]
+    },
     plugins: {
       legend: {
         display: false
